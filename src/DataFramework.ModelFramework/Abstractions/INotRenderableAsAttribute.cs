fe041733691cd7bec0abaac142d2c9287da59e59
Paul Deen <paul.deen@dutchworkz.nl>
2021-12-04 13:09:31 +0100

    Removed commented code

--- src/DataFramework.ModelFramework/Abstractions/INotRenderableAsAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace DataFramework.ModelFramework.Abstractions
-//{
-//    /// <summary>
-//    /// Marks a metadata item to skip render as attribute.
-//    /// </summary>
-//    public interface INotRenderableAsAttribute
-//    {
-//    }
-//}
