22cb0ff38ba2e9144fac6877afc560a7bec7bb5e
Paul Deen <paul.deen@dutchworkz.nl>
2021-12-30 08:54:50 +0100

    Minor: Formatted code

--- src/DataFramework.Core/Metadata.cs
+++ src/DataFramework.Core/Metadata.cs
@@ -19,6 +19,8 @@ namespace DataFramework.Core
             Value = value;
         }
 
-        public override string ToString() => $"[{Name}] = [{Value}]";
+        public override string ToString() => Value == null
+            ? $"[{Name}] = NULL"
+            : $"[{Name}] = [{Value}]";
     }
 }
