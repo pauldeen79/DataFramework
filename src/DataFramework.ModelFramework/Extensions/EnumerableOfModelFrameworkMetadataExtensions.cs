7ebb62a9b566e530b4aa05861c10604da1a7db6e
Paul Deen <paul.deen@dutchworkz.nl>
2022-01-20 20:46:01 +0100

    Upgraded to latest version of ModelFramework

--- src/DataFramework.ModelFramework/Extensions/EnumerableOfModelFrameworkMetadataExtensions.cs
+++ src/DataFramework.ModelFramework/Extensions/EnumerableOfModelFrameworkMetadataExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using DataFramework.Abstractions;
-using ModelFramework.Common.Default;
+using ModelFramework.Common;
 
 namespace DataFramework.ModelFramework.Extensions
 {
