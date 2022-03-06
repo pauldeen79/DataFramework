176bcc9a84061271f73d3c91fa83a1a16fa4e3af
Paul Deen <paul.deen@dutchworkz.nl>
2022-03-06 14:35:44 +0100

    Regenerated code using multiple files

--- src/DataFramework.Core/FieldInfo.generated.cs
+++ src/DataFramework.Core/FieldInfo.generated.cs
@@ -15,76 +15,6 @@ using System.Text;
 namespace DataFramework.Core
 {
 #nullable enable
-    public partial record DataObjectInfo : DataFramework.Abstractions.IDataObjectInfo
-    {
-        public CrossCutting.Common.ValueCollection<DataFramework.Abstractions.IFieldInfo> Fields
-        {
-            get;
-        }
-
-        public string? AssemblyName
-        {
-            get;
-        }
-
-        public string? TypeName
-        {
-            get;
-        }
-
-        public string Name
-        {
-            get;
-        }
-
-        public string? Description
-        {
-            get;
-        }
-
-        public string? DisplayName
-        {
-            get;
-        }
-
-        public bool IsVisible
-        {
-            get;
-        }
-
-        public bool IsReadOnly
-        {
-            get;
-        }
-
-        public bool IsQueryable
-        {
-            get;
-        }
-
-        public CrossCutting.Common.ValueCollection<DataFramework.Abstractions.IMetadata> Metadata
-        {
-            get;
-        }
-
-        public DataObjectInfo(System.Collections.Generic.IEnumerable<DataFramework.Abstractions.IFieldInfo> fields, string? assemblyName, string? typeName, string name, string? description, string? displayName, bool isVisible, bool isReadOnly, bool isQueryable, System.Collections.Generic.IEnumerable<DataFramework.Abstractions.IMetadata> metadata)
-        {
-            this.Fields = new CrossCutting.Common.ValueCollection<DataFramework.Abstractions.IFieldInfo>(fields);
-            this.AssemblyName = assemblyName;
-            this.TypeName = typeName;
-            this.Name = name;
-            this.Description = description;
-            this.DisplayName = displayName;
-            this.IsVisible = isVisible;
-            this.IsReadOnly = isReadOnly;
-            this.IsQueryable = isQueryable;
-            this.Metadata = new CrossCutting.Common.ValueCollection<DataFramework.Abstractions.IMetadata>(metadata);
-            System.ComponentModel.DataAnnotations.Validator.ValidateObject(this, new System.ComponentModel.DataAnnotations.ValidationContext(this, null, null), true);
-        }
-    }
-#nullable restore
-
-#nullable enable
     public partial record FieldInfo : DataFramework.Abstractions.IFieldInfo
     {
         public string Name
@@ -183,26 +113,5 @@ namespace DataFramework.Core
         }
     }
 #nullable restore
-
-#nullable enable
-    public partial record Metadata : DataFramework.Abstractions.IMetadata
-    {
-        public string Name
-        {
-            get;
-        }
-
-        public object? Value
-        {
-            get;
-        }
-
-        public Metadata(string name, object? value)
-        {
-            this.Name = name;
-            this.Value = value;
-            System.ComponentModel.DataAnnotations.Validator.ValidateObject(this, new System.ComponentModel.DataAnnotations.ValidationContext(this, null, null), true);
-        }
-    }
-#nullable restore
 }
+
