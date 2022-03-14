f4004f771300e19135577da1114d33de879a915b
Paul Deen <paul.deen@dutchworkz.nl>
2022-03-14 07:39:48 +0100

    Minor: Fixed path problem in code generation on Linux and Mac OS

--- src/DataFramework.CodeGeneration/CodeGenerationProviders/Builders.cs
+++ src/DataFramework.CodeGeneration/CodeGenerationProviders/Builders.cs
@@ -2,7 +2,7 @@
 
 public class Builders : DataFrameworkCSharpClassBase, ICodeGenerationProvider
 {
-    public override string Path => "DataFramework.Core\\Builders";
+    public override string Path => "DataFramework.Core/Builders";
     public override string DefaultFileName => "Builders.generated.cs";
 
     public override object CreateModel()
