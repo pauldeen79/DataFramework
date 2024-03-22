1fd28d1b30a4134cf3f73b7fcb632a8f390daeb4
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-22 11:48:32 +0100

    Added suppressed test because I need to fix Shared validation in ClassFramework and then replace code generation in DIalogFramework (and add ValidataObject to the models)

---
+++ src/DataFramework.Core.Tests/Builders/DataObjectInfoBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿namespace DataFramework.Core.Tests.Builders;
+
+public class DataObjectInfoBuilderTests
+{
+    private static DataObjectInfoBuilder CreateSut() => new DataObjectInfoBuilder();
+
+    [Fact(Skip = "Can only pass after fixing problem in ClassFramework, and then replace ModelFramework code generation in DataFramework with ClassFramework")]
+    public void Can_Validate_Recursively()
+    {
+        // Arrange
+        var sut = CreateSut().AddFields(new FieldInfoBuilder());
+
+        // Act
+        var validationResults = new List<ValidationResult>();
+        var success = sut.TryValidate(validationResults);
+
+        // Assert
+        success.Should().BeFalse();
+        validationResults.Should().HaveCount(2); //both the validation errors in DataObjectInfo and FieldInfo
+    }
+}
--- src/DataFramework.Core.Tests/GlobalUsings.cs
+++ src/DataFramework.Core.Tests/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using System;
+global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
-global using System.Linq;
+global using CrossCutting.Common.Extensions;
 global using DataFramework.Core.Builders;
 global using FluentAssertions;
 global using Xunit;
