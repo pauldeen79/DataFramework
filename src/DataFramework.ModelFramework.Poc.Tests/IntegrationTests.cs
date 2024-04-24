10f40028ce7c3e44287e98d81bd5d9ed524fa0cb
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-24 17:36:20 +0200

    Fixed DI registration after upgrade

--- src/DataFramework.ModelFramework.Poc.Tests/IntegrationTests.cs
+++ src/DataFramework.ModelFramework.Poc.Tests/IntegrationTests.cs
@@ -17,7 +17,7 @@ public sealed partial class IntegrationTests : IDisposable
 
         ServiceProvider = new ServiceCollection()
             .AddPdcNet()
-            .AddSingleton(Connection)
+            .AddSingleton<System.Data.Common.DbConnection>(Connection)
             .BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
         Scope = ServiceProvider.CreateScope();
         Repository = Scope.ServiceProvider.GetRequiredService<ICatalogRepository>();
