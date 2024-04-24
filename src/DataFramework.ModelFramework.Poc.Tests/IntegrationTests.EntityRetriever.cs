a2316416be79a06c49836998de84ffa91fcf30f3
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-24 17:47:12 +0200

    Added async tests

--- src/DataFramework.ModelFramework.Poc.Tests/IntegrationTests.EntityRetriever.cs
+++ src/DataFramework.ModelFramework.Poc.Tests/IntegrationTests.EntityRetriever.cs
@@ -20,4 +20,23 @@ public sealed partial class IntegrationTests
         actual.Should().ContainSingle();
         actual.First().IsExistingEntity.Should().BeTrue(); //set from CatalogEntityMapper
     }
+
+    [Fact]
+    public async Task Can_Query_Database_Using_Command_Async()
+    {
+        // Arrange
+        Connection.AddResultForDataReader(new[] { new Catalog(1, "Diversen cd 1", DateTime.Today, DateTime.Now, DateTime.Now, "0000-0000", "CDT", "CDR", "CD-ROM", 1, 2, true, true, @"C:\", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null) });
+
+        // Act
+        var actual = await Retriever.FindManyAsync(new SelectCommandBuilder()
+            .Select("*")
+            .From("Catalog")
+            .Where("LEFT(Name, 11) = @p0")
+            .AppendParameter("p0", "Diversen cd")
+            .Build());
+
+        // Assert
+        actual.Should().ContainSingle();
+        actual.First().IsExistingEntity.Should().BeTrue(); //set from CatalogEntityMapper
+    }
 }
