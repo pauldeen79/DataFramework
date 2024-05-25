015f22133ac9c64f37869e20e4703493c49224db
Paul Deen <paul.deen@dutchworkz.nl>
2024-05-25 16:51:22 +0200

    Refactored integration test, so we generate code using ICodeGenerationEngine instead of ITemplateEngine - one abstraction level higher

--- src/DataFramework.Pipelines.Tests/IntegrationTests.cs
+++ src/DataFramework.Pipelines.Tests/IntegrationTests.cs
@@ -4,55 +4,48 @@ public sealed class IntegrationTests : TestBase
 {
     public IntegrationTests()
     {
-        var templateFactory = Fixture.Freeze<ITemplateFactory>();
-        var templateProviderPluginFactory = Fixture.Freeze<ITemplateComponentRegistryPluginFactory>();
+        var assemblyInfoContextService = Fixture.Freeze<IAssemblyInfoContextService>();
         Provider = new ServiceCollection()
             .AddTemplateFramework()
             .AddTemplateFrameworkChildTemplateProvider()
+            .AddTemplateFrameworkCodeGeneration()
+            .AddTemplateFrameworkRuntime()
             .AddCsharpExpressionDumper()
             .AddClassFrameworkTemplates()
             .AddParsers()
             .AddPipelines()
             .AddDataFrameworkPipelines()
-            .AddScoped(_ => templateFactory)
-            .AddScoped(_ => templateProviderPluginFactory)
+            .AddScoped(_ => assemblyInfoContextService)
             .BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
         Scope = Provider.CreateScope();
-        templateFactory.Create(Arg.Any<Type>()).Returns(x => Scope.ServiceProvider.GetRequiredService(x.ArgAt<Type>(0)));
     }
 
     [Fact]
     public async Task Can_Create_Code_For_Immutable_Class_Entity_With_ConcurrencyChecks()
     {
         // Arrange
-        var sourceModel = new DataObjectInfoBuilder().WithName("MyEntity").AddFields(new FieldInfoBuilder().WithName("MyField").WithTypeName(typeof(int).FullName!)).Build();
+        var sourceModel = new DataObjectInfoBuilder()
+            .WithName("MyEntity")
+            .AddFields(new FieldInfoBuilder().WithName("MyField").WithTypeName(typeof(int).FullName!))
+            .Build();
         var settings = new PipelineSettingsBuilder()
             .WithEntityClassType(EntityClassType.ImmutableClass)
             .WithDefaultEntityNamespace("MyNamespace")
             .WithConcurrencyCheckBehavior(ConcurrencyCheckBehavior.AllFields)
             .Build();
         var context = new EntityContext(sourceModel, settings, CultureInfo.InvariantCulture);
-        var entityPipeline = Scope!.ServiceProvider.GetRequiredService<IPipelineBuilder<EntityContext>>().Build();
-        var templateEngine = Scope.ServiceProvider.GetRequiredService<ITemplateEngine>();
-        var templateFactory = Scope.ServiceProvider.GetRequiredService<ITemplateFactory>();
-        TemplateTypeIdentifier identifier = new TemplateTypeIdentifier(typeof(CsharpClassGenerator), templateFactory);
-        var builder = new System.Text.StringBuilder();
+        var entityPipeline = Scope!.ServiceProvider.GetRequiredService<IPipeline<EntityContext>>();
+        var generationEnvironment = new StringBuilderEnvironment();
+        var codeGenerationSettings = new CodeGenerationSettings(string.Empty, "GeneratedCode.cs", true);
+        var codeGenerationEngine = Scope.ServiceProvider.GetRequiredService<ICodeGenerationEngine>();
 
         // Act
         var result = await entityPipeline.Process(context);
         result.ThrowIfInvalid();
-        var viewModel = new CsharpClassGeneratorViewModel
-        {
-            Model = [context.Builder.Build()],
-            Settings = new CsharpClassGeneratorSettingsBuilder()
-                .WithEncoding(Encoding.UTF8)
-                .WithCultureInfo(CultureInfo.InvariantCulture)
-                .Build()
-        };
-        await templateEngine.Render(new RenderTemplateRequest(identifier, viewModel, builder), CancellationToken.None);
+        await codeGenerationEngine.Generate(new MyGenerator(context.Builder.Build()), generationEnvironment, codeGenerationSettings, CancellationToken.None);
 
         // Assert
-        builder.ToString().Should().Be(@"using System;
+        generationEnvironment.Builder.ToString().Should().Be(@"using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -108,5 +101,29 @@ namespace MyNamespace
     }
 }
 ");
+    }
+
+    private sealed class MyGenerator : CsharpClassGeneratorCodeGenerationProviderBase
+    {
+        private readonly TypeBase _model;
+
+        public MyGenerator(TypeBase model)
+            => _model = model;
+
+        public override string Path
+            => string.Empty;
+        public override bool RecurseOnDeleteGeneratedFiles
+            => false;
+        public override string LastGeneratedFilesFilename
+            => string.Empty;
+        public override Encoding Encoding
+            => Encoding.UTF8;
+        public override CsharpClassGeneratorSettings Settings
+            => new CsharpClassGeneratorSettingsBuilder()
+                .WithCultureInfo(CultureInfo.InvariantCulture)
+                .WithEncoding(Encoding)
+                .Build();
+        public override Task<IEnumerable<TypeBase>> GetModel()
+            => Task.FromResult<IEnumerable<TypeBase>>([_model]);
     }
 }
