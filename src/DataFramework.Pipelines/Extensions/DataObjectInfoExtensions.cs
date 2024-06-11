137a4153f20e683e215969787cbf9410a6debcc0
Paul Deen <paul.deen@dutchworkz.nl>
2024-06-11 19:49:20 +0200

    Removed unused code

--- src/DataFramework.Pipelines/Extensions/DataObjectInfoExtensions.cs
+++ src/DataFramework.Pipelines/Extensions/DataObjectInfoExtensions.cs
@@ -4,19 +4,4 @@ public static class DataObjectInfoExtensions
 {
     public static IEnumerable<FieldInfo> GetUpdateConcurrencyCheckFields(this DataObjectInfo instance, ConcurrencyCheckBehavior concurrencyCheckBehavior)
         => instance.Fields.Where(fieldInfo => fieldInfo.IsUpdateConcurrencyCheckField(concurrencyCheckBehavior));
-
-    public static string GetBuilderTypeName(this DataObjectInfo instance, PipelineSettings settings)
-    {
-        var ns = settings.DefaultBuilderNamespace
-            .WhenNullOrEmpty(settings.DefaultEntityNamespace)
-            .WhenNullOrEmpty(instance.TypeName.GetNamespaceWithDefault());
-
-        var name = string.IsNullOrEmpty(instance.TypeName)
-            ? instance.Name
-            : instance.TypeName!.GetClassName().WhenNullOrEmpty(instance.Name);
-
-        return string.IsNullOrEmpty(ns)
-            ? $"{name}Builder"
-            : $"{ns}.{name}Builder";
-    }
 }
